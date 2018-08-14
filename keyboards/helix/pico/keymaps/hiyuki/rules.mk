77a8e91c88b8a1202153ac8f5fc848336f0076a1
hiyuki2578 <tomoka319@gmail.com>
2018-08-14 20:47:03 +0900

    change rules.mk

--- keyboards/helix/pico/keymaps/hiyuki/rules.mk
+++ keyboards/helix/pico/keymaps/hiyuki/rules.mk
@@ -29,12 +29,12 @@ endef
 # Helix keyboard customize
 # you can edit follows 6 Variables
 #  jp: 以下の6つの変数を必要に応じて編集します。
-OLED_ENABLE = no            # OLED_ENABLE
+OLED_ENABLE = yes           # OLED_ENABLE
 LOCAL_GLCDFONT = no         # use each keymaps "helixfont.h" insted of "common/glcdfont.c"
 LED_BACK_ENABLE = no        # LED backlight (Enable WS2812 RGB underlight.)
 LED_UNDERGLOW_ENABLE = no   # LED underglow (Enable WS2812 RGB underlight.)
 LED_ANIMATIONS = yes        # LED animations
-IOS_DEVICE_ENABLE = no      # connect to IOS device (iPad,iPhone)
+IOS_DEVICE_ENABLE = yes     # connect to IOS device (iPad,iPhone)
 Link_Time_Optimization = no # if firmware size over limit, try this option
 
 ####  LED_BACK_ENABLE and LED_UNDERGLOW_ENABLE.
